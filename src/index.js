4ec44bc469c67d93dea4b176cd700bab033d2340
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-07-12 18:32:10 -0500

    begin working on feature transitions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+// Elements
+
 const open = document.querySelectorAll(".tab__open");
 const close = document.querySelectorAll(".tab__close");
 const submitBtn = document.querySelector(".contact-form__submit-btn");
+
+const feature1 = document.querySelector(".features-1");
+const feature2 = document.querySelector(".features-2");
+const feature3 = document.querySelector(".features-3");
+
+const featuresBtn1 = document.querySelector(".feautures-list__btn-1");
+const featuresBtn2 = document.querySelector(".feautures-list__btn-2");
+const featuresBtn3 = document.querySelector(".feautures-list__btn-3");
+
+// Event Listeners
+
+featuresBtn1.addEventListener("click", () => {
+    feature1.classList.add("flex");
+})
+
+featuresBtn2.addEventListener("click", () => {
+
+})
+
+featuresBtn3.addEventListener("click", () => {
+
+})
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
